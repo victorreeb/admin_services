54f5c3aac48feac96872d2ee9ec6d728d4b51959
victorreeb <victor.reeb@orange.fr>
2017-03-07 18:38:18 +0100

    Update connect_api.php

--- api_google_calendar/connect_api.php
+++ api_google_calendar/connect_api.php
@@ -1,5 +1,5 @@
 <?php
-$redirect_uri = 'http://localhost/admin_services/blob/master/api_google_calendar/index.php';
+$redirect_uri = 'http://localhost/admin_services/api_google_calendar/index.php';
 $response_type = 'code';
 $client_id = '986293143775-n7juk31aha6scli5j007m7j74llrg14k.apps.googleusercontent.com';
 $url = 'https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/calendar&redirect_uri='. $redirect_uri . '&response_type=' . $response_type . '&client_id=' . $client_id;
